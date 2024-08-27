85bf3ad5c6e8a1aeca9e66f1e172feeace6bf3a1
Abouzar Alsadi <81325367+abouzar-a@users.noreply.github.com>
2024-08-27 13:05:12 +0200

    Update cluster.cpp

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -75,12 +75,45 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+
+void clusterHelper(int indice, const std::vector<std::vector<float>> points, std::vector<int>& cluster, std::vector<bool>& processed, KdTree* tree, float distanceTol)
+{
+	processed[indice] = true;
+	cluster.push_back(indice);
+	
+	std::vector<int> nearest = tree->search(points[indice], distanceTol);
+	
+	for (int id : nearest)
+	{
+		if (!processed[id])
+			clusterHelper(id, points, cluster, processed, tree, distanceTol);
+	}
+}
+
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
 
 	// TODO: Fill out this function to return list of indices for each cluster
 
 	std::vector<std::vector<int>> clusters;
+	std::vector<bool> processed(points.size(), false);
+	
+	int i = 0;
+	
+	while (i < points.size())
+	{
+		if (processed[i])
+		{
+			i++;
+			continue;
+		}
+		
+		std::vector<int> cluster;
+		clusterHelper(i, points, cluster, processed, tree, distanceTol);
+		
+		clusters.push_back(cluster);
+		i++;
+	}
  
 	return clusters;
 
@@ -122,6 +155,7 @@ int main ()
   	auto startTime = std::chrono::steady_clock::now();
   	//
   	std::vector<std::vector<int>> clusters = euclideanCluster(points, tree, 3.0);
+	
   	//
   	auto endTime = std::chrono::steady_clock::now();
   	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
